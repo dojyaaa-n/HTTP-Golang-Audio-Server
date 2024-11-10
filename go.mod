b00a093ae2f843c2b9802ea6192eda5384d6f588
dojyaaan <67079979+dojyaaa-n@users.noreply.github.com>
2024-11-10 19:16:15 +0100

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module HTTP-Golang-Audio-Server
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,1 +1,40 @@
+package main
 
+import (
+	"log"
+	"io"
+	"os"
+	"net/http"
+)
+
+var fname = "audio.aac"
+
+func main() {
+	http.HandleFunc("/stream", stream)
+
+	log.Println("Listening on port 8080...")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func stream(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "audio/mpeg")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
+	log.Printf("%s has connected to the audio stream\n", r.Host)
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
